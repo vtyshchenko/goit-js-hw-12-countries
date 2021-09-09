bc6506578259ca73d35bab25a8b6f16a33bfadda
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-09-09 22:10:31 +0300

    refactoring

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-export default function fetchCountries(searchQuery) {
+export default async function fetchCountries(searchQuery) {
   if (searchQuery.length === 0) {
     return;
   }
@@ -6,16 +6,14 @@ export default function fetchCountries(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
   const myRequest = new Request(url);
 
-  return fetch(myRequest)
-    .then(response => {
-      console.dir(response);
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('Something went wrong on api server!');
-      }
-    })
-    .catch(error => {
-      console.log(`Something went wrong: ${error}!`);
-    });
+  try {
+    const response = await fetch(myRequest);
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error('Something went wrong on api server!');
+    }
+  } catch (error) {
+    console.log(`Something went wrong: ${error}!`);
+  }
 }
