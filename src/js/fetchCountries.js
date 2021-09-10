eb3cb33fbf587de68d97bc5e5b1ef5e062cc152c
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-09-10 14:18:45 +0300

    refactoring

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-import { info, error } from '../../node_modules/@pnotify/core/dist/PNotify.js';
+import { error } from '../../node_modules/@pnotify/core/dist/PNotify.js';
 
 export default async function fetchCountries(searchQuery) {
   if (searchQuery.length === 0) {
@@ -9,19 +9,7 @@ export default async function fetchCountries(searchQuery) {
   const myRequest = new Request(url);
 
   try {
-    const response = await fetch(myRequest);
-
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      if (response.status === 404) {
-        info({
-          text: 'Nothing found!',
-        });
-      } else {
-        throw new Error(`Something went wrong on api server! Response status ${response.status}`);
-      }
-    }
+    return await fetch(myRequest);
   } catch (err) {
     error({ text: `Something went wrong: ${err}!` });
   }
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,24 @@
-import ref from './ref.js';
-import fetchCountries from './fetchCountries.js';
 import { info, error, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyDesktop from '@pnotify/desktop';
-import { defaults } from '@pnotify/core';
+import { defaults, Stack } from '@pnotify/core';
+
+import ref from './ref.js';
+import fetchCountries from './fetchCountries.js';
 import countryCardTempl from '../templates/country.hbs';
 import countryListTempl from '../templates/countryList.hbs';
 
-const PNotifyFontAwesome5Fix = require('@pnotify/font-awesome5-fix');
-const PNotifyFontAwesome5 = require('@pnotify/font-awesome5');
+// const PNotifyFontAwesome5Fix = require('@pnotify/font-awesome5-fix');
+// const PNotifyFontAwesome5 = require('@pnotify/font-awesome5');
 defaultModules.set(PNotifyDesktop, {});
-defaultModules.set(PNotifyFontAwesome5Fix, {});
-defaultModules.set(PNotifyFontAwesome5, {});
-defaults.delay = 2000;
+// defaultModules.set(PNotifyFontAwesome5Fix, {});
+// defaultModules.set(PNotifyFontAwesome5, {});
+defaults.delay = 8000;
 defaults.styling = 'material';
-defaults.mode = 'light';
+defaults.stack.dir1 = 'down';
+defaults.stack.dir2 = 'left';
+// defaults.mode = 'light';
+console.log(defaults);
+const myStack = new Stack({ dir1: 'down', firstpos1: 25 });
 
 let debounce = require('lodash.debounce');
 const { countryFilterRef, countryInfoRef } = ref;
@@ -23,14 +28,30 @@ function onInput(event) {
   const promiseFetchCountry = fetchCountries(event.target.value);
 
   promiseFetchCountry
+    .then(response => {
+      if (response === undefined) {
+        return;
+      }
+
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        if (response.status === 404) {
+          error({
+            text: 'Nothing found!',
+          });
+        } else {
+          throw new Error(`Something went wrong on api server! Response status ${response.status}`);
+        }
+      }
+    })
     .then(data => {
-      console.log('data', data);
+      countryInfoRef.innerHTML = '';
       if (data && data.length > 10) {
         info({
           text: 'Too many matches found. Please enter a more specific query!',
         });
       } else {
-        countryInfoRef.innerHTML = '';
         if (data) {
           countryInfoRef.innerHTML = createMarcup(data);
         }
